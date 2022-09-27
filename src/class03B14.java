532a9146b31a3e975ec83f97c93a66cff20ed206
svidya10 <svidya10@gmail.com>
2022-09-27 16:50:17 -0700

    second commit

--- src/class03B14.java
+++ src/class03B14.java
@@ -2,6 +2,7 @@
 public class class03B14 {
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
+		System.out.println("hey there is a change in my project");
 	}
 
 }
