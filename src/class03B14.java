4170d89af4f229c046299110e103798e38e65a7d
svidya10 <svidya10@gmail.com>
2022-09-27 17:07:02 -0700

    third commit

--- src/class03B14.java
+++ src/class03B14.java
@@ -3,6 +3,7 @@ public class class03B14 {
 	public static void main(String[] args) {
 		System.out.println("hey this is a git class");
 		System.out.println("hey there is a change in my project");
+		System.out.println("how are you this is a new version");
 	}
 
 }
